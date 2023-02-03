b64e346c1481424267f42e4fff5e9cf92f1c0210
vahdian <germanpalero@gmail.com>
2023-02-03 20:08:51 +0100

    mobile v1 soulof

--- src/components/socialsComponent/Socials.jsx
+++ src/components/socialsComponent/Socials.jsx
@@ -7,15 +7,21 @@ import useWindowSize from "../../hooks/useWindowSize";
 
 export default function Socials() {
   const width = useWindowSize();
+  const soul = require("../../assets/soul.png");
   return (
-    <div className="absolute sm:left-0 top-0 sm:top-auto space-x-8 sm:space-x-0 text-black mt-6 sm:mt-0 cursor-pointer flex sm:flex-col justify-center items-center sm:h-screen w-full sm:w-auto sm:ml-6">
-      {width < 900 && <div className="mb-4 text-sm">SOUL OF KUMAMI</div>}
+    <div className="absolute sm:left-0 top-0 sm:top-auto space-x-8 sm:space-x-0 text-black mt-6 sm:mt-0 cursor-pointer flex sm:flex-col justify-center items-center sm:h-screen w-full sm:w-auto sm:ml-6 overflow-hidden">
+      {width < 900 && <img className="mb-4 text-sm w-32" src={soul} />}
       <a
         href="https://www.instagram.com/soulofkumami"
         target="_blank"
         rel="noopener noreferrer"
       >
-        <img src={Instagram} alt="" className="h-6 mb-4" id="instagram" />
+        <img
+          src={Instagram}
+          alt=""
+          className="h-4 sm:h-6 mb-4"
+          id="instagram"
+        />
       </a>
       <a
         href="https://www.linkedin.com/in/germanpalero/"
@@ -23,14 +29,14 @@ export default function Socials() {
         rel="noopener noreferrer"
         id="linkedin"
       >
-        <img src={LinkedIn} alt="" className="h-6 mb-4" id="linkedin" />
+        <img src={LinkedIn} alt="" className="h-4 sm:h-6 mb-4" id="linkedin" />
       </a>
       <a
         href="https://www.germanpalerocv.netlify.com"
         target="_blank"
         rel="noopener noreferrer"
       >
-        <img src={CV} alt="" className="h-6 mb-4" id="cv" />
+        <img src={CV} alt="" className="h-4 sm:h-6 mb-4" id="cv" />
       </a>
     </div>
   );
