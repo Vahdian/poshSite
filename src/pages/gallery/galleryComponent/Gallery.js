1fd75ff46a3950767f7401f4d5cfe026800bcf9a
vahdian <germanpalero@gmail.com>
2023-02-10 22:24:55 +0100

    desktop margins correctedd

--- src/pages/gallery/galleryComponent/Gallery.js
+++ src/pages/gallery/galleryComponent/Gallery.js
@@ -3,8 +3,8 @@ import "./Gallery.css";
 export default function Gallery({ images }) {
   const photos = images;
   return (
-    <div className="flex justify-center items-center h-screen flex-col mt-20 mb-12 ">
-      <div className="flex flex-col sm:grid sm:grid-cols-3 gap-3 sm:gallery overflow-scroll">
+    <div className="flex justify-center items-center h-screen flex-col mt-20 sm:mt-0 sm:mb-0 mb-12 ">
+      <div className="flex flex-col sm:grid sm:grid-cols-3 gap-3 sm:gallery overflow-scroll sm:overflow-hidden">
         {photos.map((photo, i) => (
           <div className="enlarge" key={i}>
             <img src={photo.link} className="imageGallery resize" alt="" />
